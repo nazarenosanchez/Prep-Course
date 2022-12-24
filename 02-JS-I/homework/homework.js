4a2df5b81b27d5f31a27740b380a94da2aba2453
nazarenosanchez <120763159+nazarenosanchez@users.noreply.github.com>
2022-12-23 22:25:03 -0500

    CAMBIOS EN EL REPOSITORIO

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "HOLA ME LLAMO NAZARENO Y SERÉ UN GRAN PROFESIONAL ";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
